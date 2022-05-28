de8d965cbe161dc7f4706b0352c945c09ed846a1
methoxy-n <90898567+methoxy-n@users.noreply.github.com>
2022-05-28 17:24:20 +0500

    Add files via upload

--- compress.py
+++ compress.py
@@ -14,3 +14,28 @@ def compress():
         print("File compressed")
         exit()
     content = raw.read()
+    counter = {}
+    for letter in content:
+        if counter.get(chr(letter)) is None:
+            counter[chr(letter)] = 1
+        else:
+            counter[chr(letter)] += 1
+    counter = sorted(counter.items(),key = lambda x: x[1])
+
+    nodes = []
+    for item in counter:
+        nodes.append(BinaryNode(content = item[0], prob = item[1]))
+
+    while len(nodes) > 1:
+
+        left = nodes[0]
+        right = nodes[1]
+        right.code = 0
+        left.code = 1
+        new_node = BinaryNode(prob = left.prob + right.prob, content = right.content + left.content, left=left, right=right)
+        nodes.remove(right)
+        nodes.remove(left)
+        nodes.append(new_node)
+        nodes = sorted(nodes, key = lambda x: x.prob)
+
+    print(nodes[0].get_dict())
---
+++ decompress.py
@@ -0,0 +1,19 @@
+import utils
+import pathlib
+import sys
+
+def decompress():
+    try:
+        name = sys.argv[1]
+        raw = open(name, "rb")
+    except IndexError:
+        print("Choose file you want to decompress")
+        exit()
+    if pathlib.Path(name).suffix != ".hfcd":
+        print("File is not compressed yet")
+        exit()
+    content = raw.read()
+    utils.print_hashssum(content)
+
+    if __name__ == '__main__':
+    decompress()
