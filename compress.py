ad4ce5dc02a0b11296c98ea3634a21ad0c65fb78
methoxy-n <90898567+methoxy-n@users.noreply.github.com>
2022-05-29 18:31:02 +0500

    Supports unicode and works

--- compress.py
+++ compress.py
@@ -15,10 +15,10 @@ def compress():
     content = raw.read()
     counter = {}
     for letter in content:
-        if counter.get(chr(letter)) is None:
-            counter[chr(letter)] = 1
+        if counter.get(letter) is None:
+            counter[letter] = 1
         else:
-            counter[chr(letter)] += 1
+            counter[letter] += 1
     
 
     node = utils.get_tree(counter)
@@ -43,20 +43,14 @@ def compress():
     
     output.write(empty_bits.to_bytes(1, byteorder="big"))
     for key in counter:
-        output.write(key.encode())
+        output.write(key.to_bytes(1, byteorder="big"))
         
-        a = counter[key]
-        counter_size = 0
-        while a > 0:
-            a //= 255
-            counter_size += 1
-        output.write(counter_size.to_bytes(1, byteorder="big"))
-        output.write(counter[key].to_bytes(counter_size, byteorder="big"))
+       output.write(counter[key].to_bytes(4, byteorder="big"))
     output.write(b'\x00')
     
     buffer = "0" * empty_bits
     for word in content:
-        buffer += codes[chr(word)]
+        buffer += codes[word]
         
         while len(buffer) >= 8:
             output.write(int(buffer[7::-1], 2).to_bytes(1, byteorder='big'))
